3869e0e3adbafa09231c4836b1b73890ebfd393f
Kbreab Tsegai <kibtsegai@gmail.com>
2017-02-13 08:23:21 +0200

    added travis

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ const courseValue = numeral(1000).format('$0, 0.00');
 const para = document.createElement("h2");
 const att = document.createAttribute('id');
 att.value = "greet";
-para.setAttributeNode(att);                // Create a <p> node
-const t = document.createTextNode(`I would pay ${courseValue} for this course!!`);      // Create a text node
+para.setAttributeNode(att);                // Create a <p> node `I would pay ${courseValue} for this course!!`
+const t = document.createTextNode("hi kibre");      // Create a text node
 para.appendChild(t);                                          // Append the text to <p>
 document.getElementById("myDIV").appendChild(para); 
 //debugger;
--- src/index.test.js
+++ src/index.test.js
@@ -12,9 +12,9 @@ describe('index.html', () => {
     it("should say hello world", (done) => {
         const index = fs.readFileSync('./src/index.html', 'utf-8');
         jsdom.env(index, (err, window) => {
-            if(err) console.log(err);
-            const element = window.document.getElementById('greet');
-            expect(element.innerHTML).to.equal("hi");
+            if(err) console.log(err); //eslint-disable-line no-console
+            const element = window.document.getElementsByTagName('h1')[0]; 
+            expect(element.innerHTML).to.equal("Hi kebari wedi haras");
             done();
             window.close();
         })
